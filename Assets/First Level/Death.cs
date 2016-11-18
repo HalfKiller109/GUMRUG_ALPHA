7bcb60662dd5c2d603ece6d7f9c80b39b9fe4ef4
Jonathan Durr <johnny.durr357@gmail.com>
2016-11-18 11:25:58 -0500

    Tried to make a death animation

--- Assets/First Level/Death.cs
+++ Assets/First Level/Death.cs
@@ -3,24 +3,26 @@ using System.Collections;
 
 public class Death : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public character hero_Test = new character();
 
-     
+    // Use this for initialization
+    void Start () {
 
-}
+        hero_Test.rigidbody = gameObject.GetComponent<Rigidbody2D>();
+       
+
+    }
     public GameObject SpawnPoint;
     
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.transform.tag == "Death Animation")
-        {
-            this.transform.position = SpawnPoint.transform.position;
-        }
-    }
-
+   
     // Update is called once per frame
-    void Update () {
-	
+    void Update ()
+    {
+        Debug.Log(gameObject.GetComponent<BoxCollider2D>().transform.tag);
+       if ( gameObject.GetComponent<BoxCollider2D>().transform.tag == "Player")
+        {
+            hero_Test.rigidbody.velocity = 
+            hero_Test.rigidbody.position = new Vector2(-29.14f, -1.4f);
+        }
 	}
 }
